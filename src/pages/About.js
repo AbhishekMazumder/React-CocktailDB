8de7a3a7682a8597625b18160df09819e85f8cc1
abhishek <abhishekmazumder69@gmail.com>
2020-09-11 19:48:59 +0530

    completed version-1.0

---
+++ src/pages/About.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function About() {
+	return (
+		<section className="section about-section">
+			<h1 className="section-title">About us</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt eveniet corporis at laborum amet ipsam repellendus quisquam! Enim aut necessitatibus delectus? Illo est, rerum reprehenderit ipsa perspiciatis ab aliquid possimus.</p>
+		</section>
+	);
+}
